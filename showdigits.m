3e9a8932b21fd30f6708d83887d2bd9824341578
iceiony <iceiony@yahoo.com>
2016-01-04 02:45:16 +0000

    add other test results for network size

--- showdigits.m
+++ showdigits.m
@@ -1,15 +1,11 @@
-%close all;
-%clear all;
 
-data = load('optdigits.tra');
-
-trarget = data(:,end);
-training = data(:,1:end-1);
-
-for digit = training'
-    imagesc(reshape(digit,8,8)');
-    pause(0.25);
+correct = 0;
+for p = 1:length(testTarg)
+    %remove 1 since index starts at 1
+    result = find(testOut{2}(p,:) == max(testOut{2}(p,:))) - 1;
+    if (result ~= testTarg(p))        
+        imagesc(reshape(digit,8,8)');
+        pause(0.25);
+    end
+    
 end
-
-clear data;
-
